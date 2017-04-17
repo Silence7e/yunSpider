27c840c0fe779430aacf37e6c57fc955dc7d6389
robin <liruibin@lvjinsuo.com>
2017-04-17 23:43:49 +0800

    add something

--- scrapyspider/middlewares.py
+++ scrapyspider/middlewares.py
@@ -63,13 +63,27 @@ class ScrapyspiderSpiderMiddleware(object):
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
 
+class RandomUserAgent(object):
+    """Randomly rotate user agents based on a list of predefined ones"""
+
+    def __init__(self, agents):
+        self.agents = agents
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings.getlist('USER_AGENTS'))
+
+    def process_request(self, request, spider):
+        #print "**************************" + random.choice(self.agents)
+        request.headers.setdefault('User-Agent', random.choice(self.agents))
+
 class ProxyMiddleware(object):
 	def process_request(self, request, spider):
 		proxy = random.choice(PROXIES)
 		if proxy['user_pass'] is not None:
 			request.meta['proxy'] = "http://%s" % proxy['ip_port']
-			encoded_user_pass = base64.encodestring(proxy['user_pass'])
-			request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+			encoded_user_pass = base64.encodebytes(bytes(proxy['user_pass'], encoding = "utf8"))
+			request.headers['Proxy-Authorization'] = 'Basic ' + str(encoded_user_pass, encoding="utf8")
 			print("**************ProxyMiddleware have pass************" + proxy['ip_port'])
 		else:
 			print("**************ProxyMiddleware no pass************" + proxy['ip_port'])
--- scrapyspider/settings.py
+++ scrapyspider/settings.py
@@ -52,9 +52,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'scrapyspider.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+# DOWNLOADER_MIDDLEWARES = {
+# 	'scrapyspider.middlewares.RandomUserAgent': 1,
+#     'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
+#     'scrapyspider.middlewares.ProxyMiddleware': 100,
+# }
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
@@ -64,9 +66,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scrapyspider.pipelines.ScrapyspiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'scrapyspider.pipelines.ScrapyspiderPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -74,7 +76,7 @@ ROBOTSTXT_OBEY = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 3
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
@@ -108,10 +110,8 @@ USER_AGENTS = [
 	"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
 ]
 PROXIES = [
-	{'ip_port': '111.11.228.75:80', 'user_pass': ''},
-	{'ip_port': '120.198.243.22:80', 'user_pass': ''},
-	{'ip_port': '111.8.60.9:8123', 'user_pass': ''},
-	{'ip_port': '101.71.27.120:80', 'user_pass': ''},
-	{'ip_port': '122.96.59.104:80', 'user_pass': ''},
-	{'ip_port': '122.224.249.122:8088', 'user_pass': ''},
+	{'ip_port': '122.245.67.96:808', 'user_pass': ''},
+	{'ip_port': '222.85.50.170:808', 'user_pass': ''},
+	{'ip_port': '183.153.28.136:808', 'user_pass': ''},
+	{'ip_port': '61.191.173.31:808', 'user_pass': ''},
 ]
